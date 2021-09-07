103d0e158b8a5f7e2725de13512d8ccb53a8a735
ShchegikhinPA <shchegolikhin_pa@mail.ru>
2021-09-07 23:57:55 +0300

    Дополнительное задание

--- Navigation/Profile /ProfileHeaderView.swift
+++ Navigation/Profile /ProfileHeaderView.swift
@@ -13,6 +13,9 @@ class ProfileHeaderView: UIView {
     let label = UILabel()
     let textFild = UITextField()
     let button = UIButton()
+    let newTextFild = UITextField()
+    
+    var text = String()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -32,8 +35,8 @@ class ProfileHeaderView: UIView {
         
         self.addSubview(button)
         button.backgroundColor = .blue
-        button.frame = CGRect(x: 16, y: self.avatar.frame.maxY + 16, width: UIScreen.main.bounds.width - 32, height: 50)
-        button.setTitle("Show status", for: .normal)
+        button.frame = CGRect(x: 16, y: self.avatar.frame.maxY + 32, width: UIScreen.main.bounds.width - 32, height: 50)
+        button.setTitle("Set status", for: .normal)
         button.layer.cornerRadius = 4
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOffset = CGSize(width: 4, height: 4)
@@ -42,12 +45,24 @@ class ProfileHeaderView: UIView {
         
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         
-        
         self.addSubview(textFild)
-        textFild.frame = CGRect(x: self.avatar.frame.maxX + 16, y: self.button.frame.minY - 34, width: 300, height: 14)
+        textFild.frame = CGRect(x: self.avatar.frame.maxX + 16, y: self.button.frame.minY - 92, width: 300, height: 20)
+        textFild.minimumFontSize = 14
         // textFild.text = "Waiting for something..."
         textFild.textColor = .gray
         textFild.placeholder = "Waiting for something..."
+        
+        self.addSubview(newTextFild)
+        newTextFild.backgroundColor = .white
+        newTextFild.frame = CGRect(x: self.avatar.frame.maxX + 16, y: self.button.frame.minY - 56, width: UIScreen.main.bounds.width - 32 - 16 - avatar.frame.height, height: 40)
+        newTextFild.minimumFontSize = 15
+        newTextFild.layer.cornerRadius = 12
+        newTextFild.layer.borderWidth = 1
+        newTextFild.layer.borderColor = UIColor.blue.cgColor
+        
+        let spacerView = UIView(frame:CGRect(x:0, y:0, width:10, height:10))
+        newTextFild.leftViewMode = UITextField.ViewMode.always
+        newTextFild.leftView = spacerView
         
     }
     
@@ -57,8 +72,9 @@ class ProfileHeaderView: UIView {
     
     @objc
     func buttonPressed() {
-        if let text = textFild.text {
-            print(text)
+        if let newText = newTextFild.text {
+            text = newText
+            textFild.text = text
         }
     }
 }
